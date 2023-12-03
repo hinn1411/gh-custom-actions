2fd43972d87812e1b08c3bcdf00309eaaf23e578
Hien Giang <quachtrieuvu@gmail.com>
2023-12-03 17:38:17 +0700

    added custom deploy action 2

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -8,7 +8,6 @@ function run() {
   const bucket = core.getInput('bucket', {required: true});
   const bucketRegion = core.getInput('bucket-region', {required: true}); // have default value
   const distFolder = core.getInput('dist-folder', {required: true});
-  AWS_ACCESS_KEY_ID = AKIAXZKQFH3XF6KXNSN4;
   // 2) Upload files
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
